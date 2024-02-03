f8cd31a2d42a215132e0494c2dadec4548fda9bc
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-02-03 14:04:20 +0900

    [Silver V] Title: 잘못된 버블정렬, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/30010. 잘못된 버블정렬/잘못된 버블정렬.py
+++ 백준/Silver/30010. 잘못된 버블정렬/잘못된 버블정렬.py
@@ -1,15 +1,17 @@
 import sys
-
+# 정연이가 작성한 코드
+# 마지막 인덱스에 최댓값이 도달 못했는데 제외시키고 버블 정렬을 반복하는 꼴
+# 고로 배열의 가장 앞자리에 최댓값이 오면 정렬을 못함
+# for i in range(N - 1, 0, -1):
+#
+#     for j in range(i-1, -1, -1):
+#         if arr[j] > arr[j + 1]:
+#             arr[j], arr[j + 1] = arr[j + 1], arr[j]
 input = sys.stdin.readline
 
 N = int(input())
 
 arr = [i for i in range(1, N+1)]
 arr.reverse()
-# 정연이가 작성한 코드
-# for i in range(N - 1, 0, -1):
-#
-#     for j in range(i-1, -1, -1):
-#         if arr[j] > arr[j + 1]:
-#             arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
 print(*arr)
