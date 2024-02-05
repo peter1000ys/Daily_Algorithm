40d0ba3c939312de2f23c9551f88183652d6fdf6
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-02-05 12:11:17 +0900

    [Silver II] Title: 나무 자르기, Time: 4808 ms, Memory: 150200 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,34 +1,28 @@
 import sys
 
 input = sys.stdin.readline
-
-# N : 나무의 수, M : 갖고 돌아갈 나무의 수
+# N : 나무 개수, M : 갖고 갈 나무 총 길이
 N, M = map(int, input().split())
-# N개 나무의 각 높이
-arr = list(map(int, input().split()))
+# trees : N 개의 나무들 각각의 높이
+trees = list(map(int, input().split()))
 
-# 이진 검색할 범위 설정
 start = 1
-end = max(arr)
+end = max(trees)
 
 while start <= end:
-    mid = (start + end) // 2
-    sliced_sum = 0
 
-    for i in arr:
-        # 잘리는 나무의 길이 합(갖고 갈 나무의 합)을 구한다
-        if i >= mid:
-            sliced_sum += i - mid
-    # 만약 갖고 갈 나무의 길이가 M보다 클 경우 잘라낸 길이가 짧았던 것
-    # 이진 탐색 범위의 시작점을 중간지점 + 1로 설정
-    if sliced_sum >= M:
-        start = mid + 1
-    # 만약 갖고 갈 나무의 길이가 M보다 작을 경우 잘라낸 길이가 길았던 것
-    # 이진 탐색 범위의 끝점을 중간지점 - 1로 설정
-    else:
-        end = mid - 1
-# 반복이 종료되는 경우가 end < start 이므로
-# 마지막 검사에서 가져갈 나무 길이가 목표한 길이 보다 작은 값이 나와
-# 거기보다 더 큰 가장 M에 근접한 경우의 H 길이(H의 최댓값)를 출력
+    height = (start + end) // 2  # 시작점과 끝점 중간값을 자를 높이 설정
+
+    tree_sum = 0  # 갖고 갈 나무 길이
+
+    for tree in trees:
+        if height < tree:  # 자를 높이가 나무 높이보다 짧은 경우
+            tree_sum += tree - height  # 자른 길이를 갖고 갈 나무에 더해줌
+        else:
+            continue
+    if tree_sum >= M:  # 만약 갖고 갈 나무가 원하던 것보다 많이 나오면
+        start = height + 1  # 높이 설정이 짧았던 것으로 시작점을 바꿔준다
+    else:  # 갖고 갈 나무가 원하던 것보다 적을 경우
+        end = height - 1  # 자를 높이 설정이 높았던 것임으로 끝점을 바꿔준다
+
 print(end)
-
