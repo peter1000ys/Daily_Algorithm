4d94cf93ad91270136ae4b17b1493e77625eb3ac
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-02-06 10:44:35 +0900

    [Silver IV] Title: 기타줄, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.py
+++ 백준/Silver/1049. 기타줄/기타줄.py
@@ -1,40 +1,29 @@
 import sys
 input = sys.stdin.readline
-"""
-결국 패키지 가격 중 가장 싼 값과 낱개의 가격 중 가장 싼 값을 가져 와서
-상황에 따라 비교하여 최소의 경우를 찾는 문제
 
-N이 6보다 큰 경우
-낱개의 최솟값 * 6 과 패키지 최솟값을 비교하여 더 낮은 가격을 선택
-
-N이 6보다 작은 경우
-낱개의 최솟값 * N 과 패키지 최솟값을 비교하여 더 낮은 가격을 선택
-"""
 N, M = map(int, input().split())
 
-package = []
-one = []
+package_cost = []
+single_cost = []
 
 for _ in range(M):
-    p, o = map(int, input().split())
-    package.append(p)
-    one.append(o)
+    p, s = map(int, input().split())
+    package_cost.append(p)
+    single_cost.append(s)
 
-min_package = min(package)
-
-
+min_package = min(package_cost)
 total = 0
 
 while N > 0:
     if N >= 6:
-        min_one = min(one) * 6
+        min_single = min(single_cost) * 6
         N -= 6
     else:
-        min_one = min(one) * N
+        min_single = min(single_cost) * N
         N -= N
 
-    if min_one < min_package:
-        total += min_one
+    if min_single < min_package:
+        total += min_single
     else:
         total += min_package
 
