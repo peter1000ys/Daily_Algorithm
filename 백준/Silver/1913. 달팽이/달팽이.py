5e3533a5e85417e997a7ff81d2f482be75571534
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-02-07 11:37:27 +0900

    [Silver III] Title: 달팽이, Time: 292 ms, Memory: 120724 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,45 +1,39 @@
 import sys
 
 input = sys.stdin.readline
-#     하 우 상 좌
+
 dr = [1, 0, -1, 0]
 dc = [0, 1, 0, -1]
-
-row, col = 0, 0
-
 N = int(input())
-
-K = int(input())
-
+M = int(input())
 
 board = [[0] * N for _ in range(N)]
 
-
-
-# 위치
 d = 0
+row, col = 0, 0
+ans_r, ans_c = 0, 0
+for num in range(N ** 2, 0, -1):
+    board[row][col] = num
 
-for i in range(N**2, 0, -1):
-    board[row][col] = i
+    if board[row][col] == M:
+        ans_r, ans_c = row+1, col+1
 
     nr = row + dr[d]
     nc = col + dc[d]
 
     if 0 <= nr < N and 0 <= nc < N and board[nr][nc] == 0:
-        row = nr
-        col = nc
+
+        row, col = nr, nc
 
     else:
-        d += 1
-        if d == 4:
+        if d < 3:
+            d += 1
+        elif d == 3:
             d = 0
         row += dr[d]
         col += dc[d]
+        continue
 
-
-for j in range(len(board)):
-    print(*board[j])
-    if K in board[j]:
-        kx = j + 1
-        ky = board[j].index(K) + 1
-print(kx, ky)
+for x in board:
+    print(*x)
+print(ans_r, ans_c)
