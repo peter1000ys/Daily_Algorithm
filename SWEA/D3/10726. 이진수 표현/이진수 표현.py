993d6157e5d7c2c0c997b56f8ee04dbc2994450f
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-02-23 12:19:50 +0900

    [D3] Title: 이진수 표현, Time: 1,321 ms, Memory: 63,880 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.py
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.py
@@ -1,7 +1,11 @@
+
 T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     temp = 'OFF'
+    # M의 이진수에서 각각 0번째 ~ 4번째 자리에 1이 있는지 확인하고
+    # 모두 1이 있을 경우 temp는 ON
+    # 하나라도 0일 경우 OFF, 반복문 종료
     for i in range(N):
         if M & (1 << i):
             temp = 'ON'
@@ -9,3 +13,6 @@ for tc in range(1, T+1):
             temp = 'OFF'
             break
     print(f'#{tc} {temp}')
+
+
+
