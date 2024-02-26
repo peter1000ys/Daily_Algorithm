a04e141a56836b2ad6def78bdd1ea0d7ae3a39cd
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-02-26 12:16:25 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 124 ms, Memory: 45,568 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -1,5 +1,5 @@
 def inorder(T):
-    if node[T]:
+    if T:
         inorder(left[T])
         print(node[T], end='')
         inorder(right[T])
@@ -7,15 +7,16 @@ def inorder(T):
 
 for tc in range(1, 11):
     N = int(input())
-    node = [0] * (N + 1)
     left = [0] * (N + 1)
     right = [0] * (N + 1)
+    node = [0] * (N + 1)
+
     for _ in range(N):
         arr = list(input().split())
         if len(arr) >= 2:
             node[int(arr[0])] = arr[1]
             if len(arr) >= 3:
-                left[int(arr[0])] = (int(arr[2]))
+                left[int(arr[0])] = int(arr[2])
                 if len(arr) == 4:
                     right[int(arr[0])] = int(arr[3])
     print(f'#{tc}', end=' ')
