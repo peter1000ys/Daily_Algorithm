810f8bb46667ca6be8577c1bc28f9b8a142e345c
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-03-03 00:21:03 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,21 +1,12 @@
-import sys
-
-input = sys.stdin.readline
-
-T = int(input())
-
-board = [[0] * 100 for _ in range(100)]
-cnt = 0
-for tc in range(T):
-    r, c = map(int, input().split())
+board = [[0 for _ in range(100)] for _ in range(100)]
+n = int(input())
+for i in range(n):
+    x, y = map(int, input().split())
     for i in range(10):
         for j in range(10):
-            nr = r + i
-            nc = c + j
-            if 0 <= nr < 100 and 0 <= nc < 100:
-                    board[nr][nc] = 1
-            else:
-                continue
+            board[x+i][y+j] += 1
+cnt = 0
 for i in board:
-    cnt += i.count(1)
-print(cnt)
+    cnt += i.count(0)
+ans = 10000 - cnt
+print(ans)
