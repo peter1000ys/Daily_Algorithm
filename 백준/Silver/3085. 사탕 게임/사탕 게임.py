477f2d2034a87c7177495efbd0ca3bb102e317cb
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-03-03 22:39:39 +0900

    [Silver II] Title: 사탕 게임, Time: 1952 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,55 +1,53 @@
-import sys
-
-input = sys.stdin.readline
-
-
-def check(arr):
-    n = len(arr)
-    ans = 1
+def check(lst):
+    max_ = 1
+    n = len(lst)
     for i in range(n):
         cnt = 1
         for j in range(1, n):
-            if arr[i][j] == arr[i][j - 1]:
+            if lst[i][j] == lst[i][j - 1]:
                 cnt += 1
             else:
                 cnt = 1
+            if cnt > max_:
+                max_ = cnt
 
-            if cnt > ans:
-                ans = cnt
+    for i in range(n):
         cnt = 1
         for j in range(1, n):
-            if arr[j][i] == arr[j - 1][i]:
+            if lst[j][i] == lst[j - 1][i]:
                 cnt += 1
             else:
                 cnt = 1
-
-            if cnt > ans:
-                ans = cnt
-    return ans
+            if cnt > max_:
+                max_ = cnt
+    return max_
 
 
 N = int(input())
-arr = [list(input().rstrip()) for _ in range(N)]
-result = 0
+arr = [list(input()) for _ in range(N)]
+
+ans = 0
 
 for i in range(N):
     for j in range(N):
+        
         if j + 1 < N:
             arr[i][j], arr[i][j + 1] = arr[i][j + 1], arr[i][j]
 
-            a = check(arr)
-            if a > result:
-                result = a
-
+            result = check(arr)
+            if result > ans:
+                ans = result
+                
             arr[i][j], arr[i][j + 1] = arr[i][j + 1], arr[i][j]
 
+for i in range(N):
+    for j in range(N):
         if i + 1 < N:
-            arr[i][j], arr[i + 1][j] = arr[i+1][j], arr[i][j]
+            arr[i][j], arr[i + 1][j] = arr[i + 1][j], arr[i][j]
 
-            a = check(arr)
-            if a > result:
-                result = a
+            result = check(arr)
+            if result > ans:
+                ans = result
+            arr[i][j], arr[i + 1][j] = arr[i + 1][j], arr[i][j]
 
-            arr[i][j], arr[i + 1][j] = arr[i+1][j], arr[i][j]
-
-print(result)
+print(ans)
