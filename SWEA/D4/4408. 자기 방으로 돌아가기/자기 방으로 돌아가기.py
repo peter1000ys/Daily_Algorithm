8a64da448e63b3119628d736e75c84ad64ec8ef1
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-03-03 22:50:16 +0900

    [D4] Title: 자기 방으로 돌아가기, Time: 168 ms, Memory: 59,460 KB -BaekjoonHub

--- SWEA/D4/4408. 자기 방으로 돌아가기/자기 방으로 돌아가기.py
+++ SWEA/D4/4408. 자기 방으로 돌아가기/자기 방으로 돌아가기.py
@@ -1,22 +1,12 @@
-"""
-corridor 발생 조건
-end / 2 값이 다른 값 start 보다 크거나 같을 경우
-
-"""
 T = int(input())
+
 for tc in range(1, T + 1):
     N = int(input())
-    hall = [0] * 400
+    cnt = [0] * 201
     for _ in range(N):
-
         s, e = map(int, input().split())
-
-        # 아랫방을 윗방으로 변경
-        if s % 2 == 0: s -= 1
-        if e % 2 == 0: e -= 1
-
-        if s > e: s, e = e, s
-        for i in range(s, e + 1):
-            hall[i] += 1
-
-    print(f'#{tc} {max(hall)}')
+        if s > e:
+            s, e = e, s
+        for i in range((s-1)//2, (e-1)//2+1):
+            cnt[i] += 1
+    print(f'#{tc} {max(cnt)}')
