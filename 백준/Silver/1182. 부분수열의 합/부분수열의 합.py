5c0a67d4d728d0a6602681115fcd8f074f1dacac
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-03-04 14:51:09 +0900

    [Silver II] Title: 부분수열의 합, Time: 428 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,17 +1,22 @@
-import sys
-
-input = sys.stdin.readline
-# 수열의 숫자 수 : N, 크기가 양수인 부분 수열 중 그 수열을 다 더한 값 : S
-N, S = map(int, input().split())
-# 주어진 수열
+path = []
+cnt = 0
+n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 
-cnt = 0
-for i in range(1, 1 << len(arr)):
-    lite_sum = []
-    for j in range(len(arr)):
-        if i & (1 << j):
-            lite_sum.append(arr[j]) # 해당 부분 수열의 합
-    if sum(lite_sum) == S:
-        cnt += 1    # 부분 수열 속 원소를 다 더한 값이 S가 되는 경우의 수 카운트
+
+def Hap(start):
+    global cnt
+
+    if path and sum(path) == s:
+        cnt += 1
+
+    if start == n:
+        return
+
+    for i in range(start, n):
+        path.append(arr[i])
+        Hap(i+1)
+        path.pop()
+
+Hap(0)
 print(cnt)
