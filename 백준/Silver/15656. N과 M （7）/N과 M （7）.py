51988e7f00a0710b3f000fe6cb27d22e9cda6b86
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-03-04 17:04:12 +0900

    [Silver III] Title: N과 M (7), Time: 2192 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,18 @@
+path = []
+
+def NnM(x):
+    if x == m:
+        print(*path, sep=' ')
+
+        return
+
+    for i in range(0, n):
+        path.append(arr[i])
+        NnM(x+1)
+        path.pop()
+
+
+n,m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+NnM(0)
