2ace7898c470b833ae87a0ba0fd2e89cb702312d
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-03-10 13:10:53 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 585 ms, Memory: 63,048 KB -BaekjoonHub

--- SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.py
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.py
@@ -1,16 +1,26 @@
 T = int(input())
+
 for tc in range(1, T+1):
-    N, M = map(int, input().split())
-    arr = [list(input()) for _ in range(N)]
+    n, m = map(int, input().split())
+    arr = [list(input()) for _ in range(n)]
     max_ = 0
-    for i in range(N-2):
-        for j in range(i+1, N-1):
-            cnt = 0
-            for k in range(i+1):
-                cnt += arr[k].count('W')
-            for k in range(i+1, j+1):
-                cnt += arr[k].count('B')
-            for k in range(j+1, N):
-                cnt += arr[k].count('R')
-            max_ = max(max_, cnt)
-    print(f'#{tc} {N*M - max_}')
+    lst = []
+    for i in range(n-2):
+
+        for j in range(i+1, n-1):
+
+            lst.append((i, j))
+
+    for a, b in lst:
+        cnt = 0
+        for x in range(a+1):
+            cnt += arr[x].count('W')
+        for y in range(a+1, b+1):
+            cnt += arr[y].count('B')
+        for z in range(b+1, n):
+            cnt += arr[z].count('R')
+        max_ = max(max_, cnt)
+
+    print(f'#{tc} {n*m - max_}')
+
+
