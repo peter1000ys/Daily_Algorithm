e7ec858f99e28888ba63d270e425b6de6f34fc9a
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-03-14 17:03:19 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -2,35 +2,30 @@ from collections import deque
 
 
 def bfs(r,c):
-    queue = deque()
-    queue.append((r, c))
     arr[r][c] = 0
+    queue = deque()
+    queue.append((r,c))
     cnt = 1
-
     while queue:
-        si, sj = queue.popleft()
-        for di, dj in ((-1, 0), (1, 0), (0,1), (0, -1)):
-            ni, nj = si + di, sj + dj
-            if 0 <= ni < n and 0 <= nj < n and arr[ni][nj] == 1:
-                arr[ni][nj] = 0
-                queue.append((ni, nj))
+        sr, sc = queue.popleft()
+        for dr, dc in ((1,0), (-1, 0), (0, 1), (0, -1)):
+            nr, nc = sr + dr, sc + dc
+            if 0 <= nr < n and 0 <= nc < n and arr[nr][nc] == 1:
+                queue.append((nr,nc))
+                arr[nr][nc] = 0
                 cnt += 1
-            else:
-                continue
     return cnt
 
 
 n = int(input())
 arr = [list(map(int, input())) for _ in range(n)]
 
-
-ans = []
+lst = []
 for i in range(n):
     for j in range(n):
         if arr[i][j] == 1:
-            ans.append(bfs(i, j))
-
-ans.sort()
-print(len(ans))
-for i in ans:
+            lst.append(bfs(i, j))
+lst.sort()
+print(len(lst))
+for i in lst:
     print(i)
