0323ed554b9de1f9b5276215b2026fd580112057
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-03-19 16:48:39 +0900

    [D4] Title: 동철이의 일 분배, Time: 7,824 ms, Memory: 75,968 KB -BaekjoonHub

--- SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.py
+++ SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.py
@@ -1,4 +1,4 @@
-def dongchil(x, used, cnt):
+def dongchil(x, cnt):
     global max_
 
     if cnt <= max_:
@@ -9,18 +9,20 @@ def dongchil(x, used, cnt):
         return
 
     for i in range(n):
-        if i in used:
+        if used[i]:
             continue
-        if x == n-1 and cnt * (arr[i][x]/100) < max_:
+        if x == n - 1 and cnt * (arr[i][x] / 100) < max_:
             continue
-        dongchil(x+1, used+[i], cnt * (arr[i][x]/100))
-
+        used[i] = 1
+        dongchil(x + 1, cnt * (arr[i][x] / 100))
+        used[i] = 0
 
 T = int(input())
 
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)]
     max_ = 0
-    dongchil(0, [], 1)
+    used = [0] * n
+    dongchil(0, 1)
     print(f'#{tc} {max_ * 100:.6f}')
