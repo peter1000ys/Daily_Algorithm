c6f0e290fd025132252830fd1427e759cf17b23a
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-03-23 01:08:12 +0900

    [Gold V] Title: 최소비용 구하기, Time: 292 ms, Memory: 57372 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,33 @@
+from heapq import heappush, heappop
+import sys
+input = sys.stdin.readline
+# 모든 도시를 들릴 필요 없네 = 다익스트라
+def dijkstra(start):
+    heap = []
+    distance[start] = 0
+    heappush(heap, (0, start))
+    while heap:
+
+        dist, node = heappop(heap)
+        if distance[node] < dist:
+            continue
+
+        for next_dist, next_node in adjl[node]:
+            new_dist = dist + next_dist
+            if new_dist < distance[next_node]:
+                distance[next_node] = new_dist
+                heappush(heap, (new_dist, next_node))
+
+n = int(input())
+m = int(input())
+INF = 1e9
+distance = [INF] * (n+1)
+adjl = [[] for i in range(n+1)]
+for i in range(m):
+    s, e, c = map(int, input().split())
+    adjl[s].append((c,e))
+
+
+a, b = map(int, input().split())
+dijkstra(a)
+print(distance[b])
