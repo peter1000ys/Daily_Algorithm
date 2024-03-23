cb363e339365d50aaf94bc3d0157a1cc214375d1
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-03-24 03:27:06 +0900

    [Gold V] Title: 두 용액, Time: 128 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+left = 0
+right = n-1
+a, b = 0, 0
+min_sum = abs(arr[left] + arr[right])
+while left < right:
+    sum_ = arr[left] + arr[right]
+
+    if abs(sum_) <= min_sum:
+        a = arr[left]
+        b = arr[right]
+        min_sum = abs(sum_)
+    # 음수 쪽이 더 작았다는 뜻
+    if sum_ <= 0:
+        left += 1
+    else:
+        right -= 1
+
+print(a, b)
