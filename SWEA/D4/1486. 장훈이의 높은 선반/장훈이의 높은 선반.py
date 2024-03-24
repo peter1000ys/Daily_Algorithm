dd644a398c748a9d5d4a1cd6a69c2b4a741eada9
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-03-24 23:47:09 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 252 ms, Memory: 70,112 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -1,20 +1,20 @@
-def back(x, sum):
+def back(x, sum_):
     global min_
-
-    if sum >= b:
-        min_ = min(min_, sum-b)
+    if sum_ >= b:
+        min_ = min(min_, sum_)
         return
 
     if x == n:
         return
 
-    back(x+1, sum+s[x])
-    back(x + 1, sum)
+    back(x+1, sum_ + s[x])
+    back(x+1, sum_)
+
 
 T = int(input())
 for tc in range(1, T+1):
     n, b = map(int, input().split())
     s = list(map(int, input().split()))
-    min_ = 10000
+    min_ = 1e9
     back(0, 0)
-    print(f'#{tc} {min_}')
+    print(f'#{tc} {min_ - b}')
