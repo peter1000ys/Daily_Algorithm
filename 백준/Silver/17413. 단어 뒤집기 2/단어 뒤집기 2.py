1fcd0a3bd43dc6ba75e4efb02c9ddc3a8f50e264
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-04-06 23:50:11 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 324 ms, Memory: 32684 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -1,45 +1,29 @@
-import sys
-input = sys.stdin.readline
-
-sentence = input().rstrip()
+sentence = list(input())
+result = ''
 stack = []
-arr = []
-temp = 0
-ans = ''
+temp = False
 for i in range(len(sentence)):
-    if sentence[i] == '<':
-        temp = 1
-        arr.append(sentence[i])
-        if stack:
-            stack.reverse()
-            ans += ''.join(stack)
-            stack = []
-
-    elif sentence[i] == ' ':
-        if temp:
-            arr.append(sentence[i])
-        else:
-            stack.reverse()
-            stack.append(sentence[i])
-            ans += ''.join(stack)
-            stack = []
-
+    if sentence[i] == ' ' and temp == False:
+        while stack:
+            result += stack.pop()
+        result += sentence[i]
+    elif i == len(sentence)-1 and temp == False:
+        result += sentence[i]
+        while stack:
+            result += stack.pop()
+    elif sentence[i] == '<':
+        while stack:
+            result += stack.pop()
+        result += sentence[i]
+        temp = True
+    elif sentence[i] == '>':
+        result += sentence[i]
+        temp = False
     else:
         if temp:
-            arr.append(sentence[i])
-            if sentence[i] == '>':
-                temp = 0
-                ans += ''.join(arr)
-                arr = []
-
-
+            result += sentence[i]
         else:
             stack.append(sentence[i])
 
-    if temp == 0 and i == len(sentence) -1:
-        stack.reverse()
-        ans += ''.join(stack)
-print(ans)
-
-
+print(result)
 
