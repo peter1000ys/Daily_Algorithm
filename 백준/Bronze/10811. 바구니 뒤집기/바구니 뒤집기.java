80c2c4c82c822128899746cb422c5b6a8a8ff140
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2024-08-19 11:38:30 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 100 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main{
+
+
+    public static void main(String [] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int [] arr = new int[n+1];
+
+        for (int i = 0; i < n+1; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            while (start < end) {
+                int tmp = arr[start];
+                arr[start++] = arr[end];
+                arr[end--] = tmp;
+            }
+        }
+
+        for (int i =1; i < n+1; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
