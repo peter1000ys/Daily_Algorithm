0ba9f5679a591a52182159287a6a8a5922acb706
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-01-25 23:22:04 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,34 +1,34 @@
 import sys
+
 input = sys.stdin.readline
 
-def dfs(i, v):
-    visited = [0] * (v+1)
-    stack = []
-    visited[i] = 1
 
-    while True:
 
-        for w in adjl[i]:
-            if visited[w] == 0:
-                stack.append(i)
-                i = w
-                visited[i] = 1
-                break
-        else:
-            if stack:
-                i =stack.pop()
-            else:
-                break
-    return visited.count(1)
+def dfs(n):
+    visit[n] = 1
 
-# V = 정점(컴퓨터) 수
-V = int(input())
-# E = 간선(컴퓨터끼리 연결) 수
-E = int(input())
-# adjl = 연결 리스트
-adjl = [[] for _ in range(V+1)]
-for _ in range(E):
-    n1, n2 = map(int, input().split())
-    adjl[n1].append(n2)
-    adjl[n2].append(n1)
-print(dfs(1, V) - 1)
+    for w in adjl[n]:
+        if visit[w] == 0:
+            visit[w] = 1
+            dfs(w)
+
+n = int(input())
+m = int(input())
+adjl = [[] for _ in range(n+1)]
+visit = [0] * (n+1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+
+    adjl[a].append(b)
+    adjl[b].append(a)
+
+dfs(1)
+
+cnt = 0
+
+for i in range(n+1):
+    if visit[i] == 1:
+        cnt += 1
+
+print(cnt-1)
