c86fb7dd51759159bf226d64711554021cea0fb3
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-01-28 14:08:28 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,17 +1,20 @@
-used = []
+import sys
 
+input = sys.stdin.readline
 
-def nm(x, path):
-    if x == m:
-        print(*path, sep=' ')
+def back(path):
+
+    if len(path) == m:
+        print(*path)
         return
 
     for i in range(1, n+1):
-        if i not in used:
-            used.append(i)
-            nm(x+1, path + [i])
-            used.pop()
+        if i not in path:
+            path.append(i)
+            back(path)
+            path.pop()
 
 
 n, m = map(int, input().split())
-nm(0, [])
+
+back([])
