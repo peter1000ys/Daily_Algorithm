5c19220532f0ec22892bc05c9a6d0c0b52bf61a1
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-01-29 08:40:22 +0900

    [Silver III] Title: N과 M (3), Time: 1968 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,13 +1,16 @@
-# 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
-# 고른 수열은 오름차순이어야 한다
-def NnM(x, arr):
-    if x == m:
-        print(*arr, sep=' ')
+import sys
+
+input = sys.stdin.readline
+
+def back(path):
+
+    if len(path) == m:
+        print(*path, sep=' ')
         return
 
     for i in range(1, n+1):
-        NnM(x+1, arr + [i])
-
+        back(path + [i])
 
 n, m = map(int, input().split())
-NnM(0, [])
+
+back([])
