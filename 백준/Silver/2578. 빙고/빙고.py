8a8438e4e17cbe3f86d0570d128c07d18fe546bf
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-03-21 16:37:26 +0900

    [Silver IV] Title: 빙고, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,69 +1,57 @@
 import sys
 
 input = sys.stdin.readline
-def bingo(point):
-    for x in range(5):
-        for y in range(5):
-            if board[x][y] == point:
-                board[x][y] = 0
 
-def bingo_check():
+def check():
     cnt = 0
-    # 가로 줄 체크
-    for x in range(5):
-        row = True
-        for y in range(5):
-            if board[x][y] == 0:
-                continue
-            else:
-                row = False
-                break
-        if row:
+    for a in range(5):
+        if arr[a].count(0) == 5:
             cnt += 1
 
-    # 세로줄 체크
-    for y in range(5):
-        col = True
-        for x in range(5):
-            if board[x][y] == 0:
-                continue
+    for a in range(5):
+        zero_cnt = 0
+        for b in range(5):
+            if arr[b][a] == 0:
+                zero_cnt += 1
             else:
-                col = False
                 break
-        if col:
+        if zero_cnt == 5:
             cnt += 1
 
-    # 대각선 체크
-    side_1 = True
-    for i in range(5):
-        if board[i][i] == 0:
-            continue
-        else:
-            side_1 = False
-    if side_1:
+    cross_cnt = 0
+    rev_cnt = 0
+    for a in range(5):
+        if arr[a][a] == 0:
+            cross_cnt += 1
+        if arr[a][4-a] == 0:
+            rev_cnt += 1
+
+    if cross_cnt == 5:
         cnt += 1
-
-    side_2 = True
-    for i in range(5):
-        if board[i][4-i] == 0:
-            continue
-        else:
-            side_2 = False
-    if side_2:
+    if rev_cnt == 5:
         cnt += 1
 
     return cnt
 
 
-board = [list(map(int, input().split())) for _ in range(5)]
-ans = [list(map(int, input().split())) for _ in range(5)]
 
+def bingo(n):
+    for x in range(5):
+        for y in range(5):
+            if arr[x][y] == n:
+                arr[x][y] = 0
+    return check()
+
+arr = [list(map(int,input().split())) for _ in range(5)]
+call = [list(map(int,input().split())) for _ in range(5)]
 cnt = 0
-for x in range(5):
-    for y in range(5):
-        bingo(ans[x][y])
+for i in range(5):
+    for j in range(5):
         cnt += 1
-        if cnt > 11:
-            if bingo_check() > 2:
-                print(cnt)
-                exit(0)
+        bingo_cnt = bingo(call[i][j])
+        if bingo_cnt >= 3:
+            print(cnt)
+            exit(0)
+
+
+
