a8a760feb37873f3da91c4633d88ba7890d3c0c6
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-03-21 14:55:07 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,31 +1,43 @@
 import sys
+
 input = sys.stdin.readline
 
-# 남자(1) : 해당 숫자 배수의 스위치만 바꿔 준다
-# 여자(2) : 해당 숫자에서 양쪽으로 대칭인 스위치들까지만 바꿔 준다.
-
-# 스위치 수
 n = int(input())
-# 각 스위치 상태
 arr = list(map(int, input().split()))
-# 학생 수
-students = int(input())
-for _ in range(students):
-    # 학생의 성별, 학생이 받은 수
-    sex, num = map(int, input().split())
+student_num = int(input())
+
+for i in range(student_num):
+    sex, num = map(int,input().split())
+
+    a = 1
+
     if sex == 1:
-        for i in range(num, n+1, num):
-            arr[i-1] = 1 - arr[i-1]
+
+        while a*num <= len(arr):
+            if arr[a*num-1] == 0:
+                arr[a*num-1] = 1
+            else:
+                arr[a*num-1] = 0
+            a += 1
     elif sex == 2:
-        idx = num - 1
-        arr[idx] = 1 - arr[idx]
-        
-        i = 1
-        while (idx-i >= 0) and (idx+i < n) and (arr[idx-i] == arr[idx+i]):
-            arr[idx-i] = 1 - arr[idx-i]
-            arr[idx+i] = 1 - arr[idx+i]
-            i += 1
-for i in range(0, len(arr), 20):
-    print(*arr[i:i+20])
 
+        if arr[num-1] == 0:
+            arr[num-1] = 1
+        else:
+            arr[num-1] = 0
 
+        while True:
+            if num-a-1 < 0 or num+a > len(arr):
+                break
+            else:
+                if arr[num-a-1] == arr[num+a-1]:
+                    if arr[num-a-1] == 0:
+                        arr[num-a-1], arr[num+a-1] = 1, 1
+                    else:
+                        arr[num-a-1], arr[num+a-1] = 0, 0
+                    a += 1
+                else:
+                    break
+
+for row in range(len(arr)//20 + 1):
+    print(*arr[20*row:20*(row+1)])
