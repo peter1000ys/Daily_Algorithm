4d86ed8298a3a4f5c29911927744ad9b8705891a
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-03-22 16:21:50 +0900

    [Silver I] Title: 배열 돌리기, Time: 3120 ms, Memory: 53276 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
@@ -5,37 +5,40 @@ input = sys.stdin.readline
 
 t = int(input())
 
+
 for _ in range(t):
-
     n, d = map(int, input().split())
-
     arr = [list(map(int, input().split())) for _ in range(n)]
     result = [[0]*n for _ in range(n)]
 
     if d < 0:
-        d = 360+d
+        d += 360
 
     if d == 360 or d == 0:
         for row in arr:
             print(*row)
+
+
     else:
         for r in range(d//45):
             for i in range(n):
                 for j in range(n):
+
                     if j == n//2:
                         result[i][n-i-1] = arr[i][j]
-                    elif i == j:
-                        result[i][n//2] = arr[i][j]
-                    elif j == n-i-1:
+
+                    elif i == n - j -1:
                         result[n//2][j] = arr[i][j]
+
                     elif i == n//2:
                         result[j][j] = arr[i][j]
+
+                    elif i == j:
+                        result[i][n//2] = arr[i][j]
+
                     else:
                         result[i][j] = arr[i][j]
-
             arr = deepcopy(result)
 
         for row in arr:
             print(*row)
-
-
