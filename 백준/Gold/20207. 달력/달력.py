1162c6b390affa3b63351c01fb1d755bb0334a0a
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-03-22 16:46:22 +0900

    [Gold V] Title: 달력, Time: 60 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.py
+++ 백준/Gold/20207. 달력/달력.py
@@ -3,33 +3,25 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-
-data = []
-board = [0] * 366
-
-for _ in range(n):
-    a, b = map(int, input().split())
-    data.append((a,b))
-
+data = [list(map(int,input().split())) for _ in range(n)]
+arr = [0] * 367
 sorted_data = sorted(data, key=lambda x : (x[0], -x[1]))
 
-for a, b in sorted_data:
-    for i in range(a, b+1):
-        board[i] += 1
+for d in sorted_data:
+    for i in range(d[0], d[1]+1):
+        arr[i] += 1
 
 cnt = 0
-max_h = 0
+max = 0
 ans = 0
+for i in arr:
+    if i != 0:
+       cnt += 1
+       if i > max:
+           max = i
 
-for x in range(1, 366):
-    if board[x] > 0:
-        cnt += 1
-        max_h = max(max_h, board[x])
-    elif board[x] == 0:
-        ans += cnt * max_h
-        cnt = 0
-        max_h = 0
-if cnt > 0:
-    ans += cnt * max_h
+    else:
+        ans += (max*cnt)
+        max, cnt = 0, 0
 
 print(ans)
