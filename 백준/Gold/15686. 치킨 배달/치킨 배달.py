17ef6f046a1e7d556480258922023e68710fc951
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-03-25 19:33:20 +0900

    [Gold V] Title: 치킨 배달, Time: 212 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,38 +1,43 @@
-def cal(lst):
-    total = 0
-    for hi, hj in ones:
-        min_ = 1000000
-        for i in lst:
-            ci, cj = chickens[i]
-            min_ = min(min_, abs(hi-ci) + abs(hj -cj))
-        total += min_
-    return total
+import sys
 
-def back(x, start, path):
-    global min_v
-    if x == m:
-        min_v = min(min_v, cal(path))
+input = sys.stdin.readline
+
+def chicken_delivery(start, final):
+
+    if len(final) == M:
+        global min_distance
+        distance = 0
+        for k in range(len(house)):
+            h_dist = 10000000
+            r1, c1 = house[k]
+            for t in final:
+                r2, c2 = chicken[t]
+                dist = abs(r1-r2) + abs(c1-c2)
+                h_dist = min(dist, h_dist)
+            distance += h_dist
+        min_distance = min(min_distance, distance)
         return
 
-    for i in range(start, len(chickens)):
-        back(x+1, i+1, path + [i])
+    for e in range(start, len(chicken)):
+        if e not in final:
+            chicken_delivery(e,final+[e])
 
 
 
-def find(x):
-    lst = []
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j] == x:
-                lst.append((i, j))
-    return lst
+N, M = map(int, input().split())
 
+town_map = [list(map(int, input().split())) for _ in range(N)]
 
-n, m = map(int, input().split())
+house = []
+chicken = []
+min_distance = 10000000
+for i in range(N):
+    for j in range(N):
+        if town_map[i][j] == 1:
+            house.append((i,j))
+        elif town_map[i][j] == 2:
+            chicken.append((i,j))
 
-arr = [list(map(int, input().split())) for _ in range(n)]
-min_v = 1000000
-ones = find(1)
-chickens = find(2)
-back(0, 0, [])
-print(min_v)
+chicken_delivery( 0, [])
+
+print(min_distance)
