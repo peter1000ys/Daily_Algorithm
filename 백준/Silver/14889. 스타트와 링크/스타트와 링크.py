780e947205a7eddf3f023c7832e4285cac941739
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-03-25 16:52:01 +0900

    [Silver I] Title: 스타트와 링크, Time: 1288 ms, Memory: 111676 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -2,32 +2,31 @@ import sys
 
 input = sys.stdin.readline
 
-def cal_team(one):
-    sum_team1, sum_team2 = 0, 0
-    for i in range(len(one)):
-        for j in range(i, len(one)):
-            sum_team1 += arr[one[i]][one[j]] + arr[one[j]][one[i]]
-    for i in range(N):
-        for j in range(i, N):
-            if i not in one and j not in one:
-                sum_team2 += arr[i][j] + arr[j][i]
-    return abs(sum_team1-sum_team2)
-
-
-def team(start, link):
+def team(start, x):
     global min_team
 
-    if len(link) == N//2:
-        min_team = min(cal_team(link), min_team)
+    if x == N//2:
+        sum_team1, sum_team2 = 0, 0
+
+        for i in range(N):
+            for j in range(N):
+                if visited[i] and visited[j] and i != j:
+                    sum_team1 += arr[i][j]
+                elif not visited[i] and not visited[j] and i != j:
+                    sum_team2 += arr[i][j]
+        min_team = min(min_team, abs(sum_team1-sum_team2))
         return
 
     for i in range(start, N):
-        if i not in link:
-            team(i, link+[i])
+        if not visited[i]:
+            visited[i] = 1
+            team(i, x+1)
+            visited[i] = 0
 # N 축구를 하기 위해 모인 사람, 짝수
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
+visited = [0] * N
 min_team = 1000000
 # N//2명으로 이루어진 팀 2개로 나눔
-team(0, [])
+team(0, 0)
 print(min_team)
