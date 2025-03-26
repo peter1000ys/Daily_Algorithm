6200140e2bc2cb25a15bf5917f8473ac7bb2fdeb
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-03-26 11:50:14 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 276 ms, Memory: 66216 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -3,27 +3,23 @@ sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
 
-def dfs(t):
+def dfs(x):
 
-    for w in adjl[t]:
-        if visit[w] == 0:
-            visit[w] = t
-            dfs(w)
-
-
+    for i in adjl[x]:
+        if parentNode[i] == 0:
+            parentNode[i] = x
+            dfs(i)
+# 노드의 개수
 n = int(input())
+
+# 연결선
 adjl = [[] for _ in range(n+1)]
-visit = [0] * (n+1)
-
-visit[1] = 1
-
 for _ in range(n-1):
-    a, b = map(int,input().split())
-
+    a,b = map(int,input().split())
     adjl[a].append(b)
     adjl[b].append(a)
 
-dfs(1)
+parentNode = [0] * (n+1)
 
-for i in range(2, n+1):
-    print(visit[i])
+dfs(1)
+print(*parentNode[2:], sep=  '\n')
