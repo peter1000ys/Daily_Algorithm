3ac87be2730356f57e79b9082d87724db9d6cf62
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-03-26 13:29:55 +0900

    [Silver I] Title: 봄버맨, Time: 80 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.py
+++ 백준/Silver/16918. 봄버맨/봄버맨.py
@@ -10,40 +10,37 @@ dc = [0, 1, 0, -1]
 
 r, c, n = map(int,input().split())
 arr = [list(input().rstrip()) for _ in range(r)]
-bomb = deque([])
 
-def bombs():
-    for i in range(r):
-        for j in range(c):
-            if arr[i][j] == 'O':
-                bomb.append((i,j))
-
-def fill_bombs():
-    for i in range(r):
-        for j in range(c):
-            if arr[i][j] == '.':
-                arr[i][j] = 'O'
 # 홀수 일 때 채우고, 짝수일 때 터트림
 # 짝수에서 터트리고 남은 폭탄을 찾고, 홀수에서는 채우기만 하고
-def boomba(x):
+def boomba(x, board):
+    if x == 1:
+        return board
+    def explode(board):
+        new_board = [['O']*c for _ in range(r)]
+        for i in range(r):
+            for j in range(c):
+                if board[i][j] == 'O':
+                    new_board[i][j] = '.'
+                    for d in range(4):
+                        qr, qc = i + dr[d], j + dc[d]
+                        if 0 <= qr < r and 0 <= qc < c:
+                            new_board[qr][qc] = '.'
+        return new_board
 
-    if x % 2 == 0:
-        bombs()
-        fill_bombs()
+    all_filled = [['O']*c for _ in range(r)]
+    first_explode = explode(board)
+    second_explode = explode(first_explode)
+
+    if x%2 == 0:
+        return all_filled
+    elif (x//2)%2 == 1:
+        return first_explode
     else:
-        while bomb:
-            sr, sc = bomb.popleft()
-            arr[sr][sc] = '.'
-            for d in range(4):
-                qr, qc = sr + dr[d], sc + dc[d]
-                if 0 <= qr < r and 0 <= qc < c:
-                    arr[qr][qc] = '.'
+        return second_explode
 
-    if x == n:
-        return
-    boomba(x+1)
-
-boomba(1)
-for row in arr:
+ans = boomba(n, arr)
+for row in ans:
     print(''.join(row))
+
 
