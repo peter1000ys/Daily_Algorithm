c1545ed69f91b77aa5def8f8eb0ac6b7730ae8a5
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-03-26 13:47:58 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,31 +1,40 @@
+import sys
 from collections import deque
+sys.setrecursionlimit(10**6)
 
+input = sys.stdin.readline
+
+dr = [1, 0, -1, 0]
+dc = [0, 1, 0, -1]
 
 def bfs(r,c):
-    arr[r][c] = 0
-    queue = deque()
-    queue.append((r,c))
     cnt = 1
+    queue = deque([(r,c)])
+    visited[r][c] = 1
     while queue:
         sr, sc = queue.popleft()
-        for dr, dc in ((1,0), (-1, 0), (0, 1), (0, -1)):
-            nr, nc = sr + dr, sc + dc
-            if 0 <= nr < n and 0 <= nc < n and arr[nr][nc] == 1:
+        for d in range(4):
+            nr, nc = sr + dr[d], sc + dc[d]
+            if 0 <= nr < n and 0 <= nc < n and arr[nr][nc] == '1' and visited[nr][nc] == 0:
+                visited[nr][nc] = 1
                 queue.append((nr,nc))
-                arr[nr][nc] = 0
                 cnt += 1
     return cnt
 
 
 n = int(input())
-arr = [list(map(int, input())) for _ in range(n)]
 
-lst = []
+arr = [list(input().rstrip()) for _ in range(n)]
+visited = [[0]*n for _ in range(n)]
+ans = []
 for i in range(n):
     for j in range(n):
-        if arr[i][j] == 1:
-            lst.append(bfs(i, j))
-lst.sort()
-print(len(lst))
-for i in lst:
-    print(i)
+        if arr[i][j] == '1' and visited[i][j] == 0:
+            house_cnt = bfs(i,j)
+            ans.append(house_cnt)
+
+print(len(ans))
+ans.sort()
+for a in ans:
+    print(a)
+
