7b9646516eb479c05752b2c2c1f66d86e03f53bc
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-03-27 23:22:19 +0900

    [Silver I] Title: 봄버맨, Time: 60 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.py
+++ 백준/Silver/16918. 봄버맨/봄버맨.py
@@ -1,46 +1,50 @@
 import sys
-from collections import deque
 sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
 
-dr = [1, 0, -1, 0]
-dc = [0, 1, 0, -1]
+dr = [0, 1, -1, 0]
+dc = [1, 0, 0, -1]
 
+def boomba(x, lst):
 
-r, c, n = map(int,input().split())
-arr = [list(input().rstrip()) for _ in range(r)]
-
-# 홀수 일 때 채우고, 짝수일 때 터트림
-# 짝수에서 터트리고 남은 폭탄을 찾고, 홀수에서는 채우기만 하고
-def boomba(x, board):
     if x == 1:
-        return board
+        return arr
+
+
     def explode(board):
         new_board = [['O']*c for _ in range(r)]
+
         for i in range(r):
             for j in range(c):
                 if board[i][j] == 'O':
                     new_board[i][j] = '.'
                     for d in range(4):
-                        qr, qc = i + dr[d], j + dc[d]
-                        if 0 <= qr < r and 0 <= qc < c:
-                            new_board[qr][qc] = '.'
+                        sr, sc = i + dr[d], j + dc[d]
+                        if 0 <= sr < r and 0 <= sc < c:
+                            new_board[sr][sc] = '.'
         return new_board
 
-    all_filled = [['O']*c for _ in range(r)]
-    first_explode = explode(board)
-    second_explode = explode(first_explode)
+    all_bomb = [['O']*c for _ in range(r)]
+    exploded = explode(lst)
+    second_exploded = explode(exploded)
 
-    if x%2 == 0:
-        return all_filled
-    elif (x//2)%2 == 1:
-        return first_explode
+    if x % 2 == 0:
+        return all_bomb
+
+    elif (x // 2) % 2 == 1:
+        return exploded
+
     else:
-        return second_explode
+        return second_exploded
+
+
+r, c, n = map(int, input().split())
+
+arr = [list(input().rstrip()) for _ in range(r)]
 
 ans = boomba(n, arr)
+
 for row in ans:
     print(''.join(row))
-
 
