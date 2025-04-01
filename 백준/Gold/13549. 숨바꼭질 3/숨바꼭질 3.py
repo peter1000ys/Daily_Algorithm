8193fefaeb3edfcf4739ff98902a62fea1766a68
peter1000ys <141493371+peter1000ys@users.noreply.github.com>
2025-04-01 13:20:48 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 164 ms, Memory: 38340 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,35 +1,31 @@
 import sys
 import heapq
 
-input = sys.stdin.readline
-
-def subin(start):
-    distance[start] = 0
+def dij(start):
     queue = []
+    arr[start] = 0
     heapq.heappush(queue, (0, start))
 
     while queue:
-        dist, now = heapq.heappop(queue)
-
-        if distance[now] < dist:
+        time, idx = heapq.heappop(queue)
+        if arr[idx] < time:
             continue
-        for n in (now+1, now-1, 2*now):
-            if 0 <= n <= limit:
-                cost = dist
-                if n != 2*now:
-                    cost = dist+1
-                if cost < distance[n]:
-                    distance[n] = cost
-                    queue.append((cost,n))
+        else:
+            for n in (idx+1,idx-1,2*idx):
+                if 0 <= n < limit:
+                    cost = time
+                    if n != 2*idx:
+                        cost = time + 1
 
-# 수빈이 위치 N, 동생의 위치 K
-N, K = map(int, input().split())
-# 걷는 다면 1초 후 1칸 씩 좌우로 이동, 순간이동은 0초 후 2배수 위치로 이동
-# 수빈이가 동생을 찾을 수 있는 가장 빠른 시간을 구하는 프로그램
+                    if cost < arr[n]:
+                        arr[n] = cost
+                        heapq.heappush(queue,(cost, n))
+
+
 INF = int(1e9)
-limit = 10**5
-distance = [INF]*(limit+1)
+limit = 10**5 + 1
+arr = [INF] * limit
 
-subin(N)
-print(distance[K])
-
+n, k = map(int, input().split())
+dij(n)
+print(arr[k])
